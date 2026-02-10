bfbf1f080c15137a69843436f1723efd94e0b53d
Aditi Rathi <rathiashok815@gmail.com>
2026-02-10 18:23:57 +0530

    Add files via upload

---
+++ Plaindrome-Number.py
@@ -0,0 +1,30 @@
+# Program: Given an integer x, return true if x is a palindrome, and false otherwise.
+
+# Example 1:
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+
+# Example 2:
+
+# Input: x = -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+# Example 3:
+
+# Input: x = 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+# x = 121
+# x = -121
+x = 10
+x = str(x)
+
+s = x[::-1]
+
+if s == x:
+    print(True)
+else:
+    print(False)
